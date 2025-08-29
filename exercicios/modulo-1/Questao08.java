b42368e232417cf5861e52bf1dd3143e60a680e4
Adriano <adrianosouzascj@gmail.com>
2025-08-28 23:41:37 -0300

    Adicionando novos exercícios

---
+++ exercicios/modulo-1/Questao08.java
@@ -0,0 +1,27 @@
+public class Questao08 {
+	public static void main(String[] args) {
+		// Declarando variáveis
+		int n1, n2, n3;
+		
+		// Capturando dados
+		System.out.println("Digite o primeiro valor inteiro: ");
+		n1 = Integer.parseInt(System.console().readLine());
+		
+		System.out.println("Digite o segundo valor inteiro: ");
+		n2 = Integer.parseInt(System.console().readLine());
+		
+		System.out.println("Digite o terceiro valor inteiro: ");
+		n3 = Integer.parseInt(System.console().readLine());		
+		
+		// Manipulando e retorno dos dados		
+		if (n1 < n2 && n1 < n3) {
+			System.out.printf("\nO valor %d é o menor valor informado.\n", n1);
+		} else if (n2 < n1 && n2 < n3) {
+			System.out.printf("O valor %d é o menor valor informado. \n", n2);
+		} else if (n3 < n1 && n3 < n2) {
+			System.out.printf("O valor %d é o menor valor informado. \n", n3);
+		} else {
+			System.out.println("Existem valores iguais.");
+		}
+	}
+}
